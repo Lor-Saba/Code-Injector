294c567f8faeacca3508e5999ba9453d966aa7e3
Lorenzo Sabatelli <lorenzo.sabatelli@teammee.com>
2017-07-03 18:05:32 +0200

    commit

--- browser_action/script/main.js
+++ browser_action/script/main.js
@@ -3,6 +3,8 @@
 
 window.addEventListener('load', function(){
 
+    var rulesCounter = 0;
+
     var editorJS    = null;
     var editorCSS   = null;
     var editorHTML  = null;
@@ -55,10 +57,7 @@ window.addEventListener('load', function(){
         switch(target.dataset.name){
 
             case 'btn-tab': 
-                var key = target.dataset.for;
-
-                //target.dataset.selected = true;
-                el.tab.dataset.selected = key;
+                el.tab.dataset.selected = target.dataset.for;
                 break;
 
             case 'btn-editor-cancel': 
@@ -69,6 +68,18 @@ window.addEventListener('load', function(){
                 el.body.dataset.editing = true;
                 break;
 
+            case 'btn-rule-delete': 
+                if (target.dataset.confirm)
+                    closest(target, '.rule').remove();
+                else{
+                    target.dataset.confirm = true;
+                    target.onmouseleave = function(){ 
+                        delete target.dataset.confirm;
+                        target.onmouseleave = null;
+                    };
+                }
+                break;
+
             case 'btn-rule-edit':
                 el.body.dataset.editing = true;
                 break;
@@ -76,13 +87,71 @@ window.addEventListener('load', function(){
         }
         
     });
+    window.addEventListener('mousedown', function(_e){
+
+        var target = _e.target;
+
+        switch(target.dataset.name){
+
+            case 'do-grip': 
+                var rule = closest(target, '.rule');
+                var ghost = stringToElement('<li class="rule-ghost"></li>');
+
+                var ruleIndex = getElementIndex(rule);
+                var Y = _e.screenY;
+
+                var evMM = function(_e){
+                    rule.style.transform = 'translateY('+(_e.screenY-Y)+'px)';
+
+                    var targetRule = closest(_e.target, '.rule');
+                    if (targetRule === null) return;
+
+                    var ghostIndex      = getElementIndex(ghost);
+                    var targetRuleIndex = getElementIndex(targetRule);
+
+                    if (targetRuleIndex < ghostIndex)
+                        targetRule.parentElement.insertBefore(ghost, targetRule);
+                    else
+                        targetRule.parentElement.insertBefore(ghost, targetRule.nextElementSibling);
+
+                    if (targetRuleIndex > ruleIndex)
+                        rule.style.marginTop = '0px';
+                    else
+                        rule.style.marginTop = '';
+                };
+                var evMU = function(){
+
+                    delete rule.dataset.dragging;
+                    rule.style.cssText = "";;
+
+                    ghost.parentElement.insertBefore(rule, ghost);
+                    ghost.remove();
+
+                    window.removeEventListener('mousemove', evMM);
+                    window.removeEventListener('mouseup', evMU);
+                };
+
+                rule.dataset.dragging = true;
+                rule.parentElement.insertBefore(ghost, rule);
+
+                window.addEventListener('mousemove', evMM);
+                window.addEventListener('mouseup', evMU);
+                break;
+
+        }
+
+    });
+
+    // DEBUG -->
 
     var ruleTmpl = getTemplate('rule');
-    for(var ind = 0; ind < 25; ind++)
-        el.rulesList.innerHTML += ruleTmpl
+    for(var ind = 0; ind < 10; ind++)
+        el.rulesList.appendChild(stringToElement(ruleTmpl, {id: rulesCounter++, name: Math.random()}));
 
     each(document.querySelectorAll('.rule .d-info'), function(){
         this.dataset.active = Math.random() > .3;
     });
+
+    // <-- DEBUG 
 
 });
--- browser_action/script/utils.js
+++ browser_action/script/utils.js
@@ -11,6 +11,58 @@ function each(_obj, _fn){
 
     if (!_obj) return;
     
-    for(var ind = 0, ln = _obj.length; ind < ln; ind++)
-        if (_fn.call(_obj[ind], ind, _obj[ind]) === false) break;
+    if (_obj.constructor === Array){
+        for(var ind = 0, ln = _obj.length; ind < ln; ind++)
+            if (_fn.call(_obj[ind], ind, _obj[ind]) === false) break;
+    }
+    if (_obj.constructor === Object){
+        for(var ind = 0, keys = Object.keys(_obj), ln = keys.length; ind < ln; ind++)
+            if (_fn.call(_obj[keys[ind]], keys[ind], _obj[keys[ind]]) === false) break;
+    }
+}
+
+function closest(_el, _fn) {
+    var el = _el;
+    var fn = _fn;
+
+    if (typeof fn === 'string'){
+        var query = fn.trim();
+        if (query[0] === '.') 
+            fn = function(_el) { return _el.classList.contains(query.substr(1)); };
+        else
+        if (query[0] === '#') 
+            fn = function(_el) { return _el.id === query.substr(1); };
+    }
+
+    while(el) if (fn(el)) return el;
+                else el = el.parentElement;
+
+    return null;
+}
+
+function stringToElement(_string, _data){
+
+    if (_data && _data.constructor === Object){
+        each(_data, function(_key){
+            _string = _string.replace(new RegExp('\{'+_key+'\}', 'g'), this);
+        });
+    }
+
+    var div = document.createElement('div');
+        div.innerHTML = _string;
+
+    return div.firstElementChild;
+}
+
+function getElementIndex(_el){
+
+    var index = 0;
+    var currEl = _el;
+
+    while(currEl.previousElementSibling){
+        currEl = currEl.previousElementSibling;
+        index++;
+    }
+
+    return index; 
 }
